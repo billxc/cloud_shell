1ed11cb2a4b7863b3a634780ca59fa4915eaec79
Xiaochen <wxc9312@gmail.com>
2023-08-06 23:39:32 +0800

    update console output

--- gitc.ps1
+++ gitc.ps1
@@ -1,5 +1,5 @@
 # use the whole command line args as the commit message, and commit
 $commit_msg_parts = $args
 $commit_msg = $commit_msg_parts -join " "
-echo "commit message: " + $commit_msg
+echo "commit message: $commit_msg"
 git commit -m $commit_msg
