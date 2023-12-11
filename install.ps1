dd0b845ac8fed24dd070c53e36e8e55138293993
Xiaochen Wu (from Dev Box) <xiaocw@microsoft.com>
2023-12-11 23:19:14 +0800

    update install

--- install.ps1
+++ install.ps1
@@ -4,5 +4,6 @@ Set-Location -Path $HOME
 # Clone the repo
 git clone https://github.com/billxc/cloud_shell.git .cloud_shell
 
+Set-Location .cloud_shell
 # Execute the init.ps1 in the .cloud_shell folder
-& '.\.cloud_shell\init.ps1'
+& 'init.ps1'
