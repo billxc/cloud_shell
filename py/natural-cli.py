199a9a7f44eb3d98b6636ef38800eb5bc6a1e45e
Xiaochen Wu <wxc9312@gmail.com>
2023-12-12 11:32:48 +0800

    migrate openai api

--- py/natural-cli.py
+++ py/natural-cli.py
@@ -8,10 +8,10 @@ from distro import name as distro_name
 import yaml
 
 operating_systems = {
-        "Linux": "Linux/" + distro_name(pretty=True),
-        "Windows": "Windows " + platform.release(),
-        "Darwin": "Darwin/MacOS " + platform.mac_ver()[0],
-    }
+    "Linux": "Linux/" + distro_name(pretty=True),
+    "Windows": "Windows " + platform.release(),
+    "Darwin": "Darwin/MacOS " + platform.mac_ver()[0],
+}
 current_platform = platform.system()
 os_name = operating_systems.get(current_platform, current_platform)
 
@@ -25,45 +25,35 @@ If multiple steps required try to combine them together.
 YOU NEED TO PROVIDE A VALID SHELL COMMAND ONLY, NO DESCRIPTIONS.
 If you cannot provide a valid shell command, add [X] to the end of your message.
 ###
-Command:""".format(shell=current_shell, os=os_name)
+Command:""".format(
+    shell=current_shell, os=os_name
+)
 # Prompt: {prompt}
+
 
 def get_response(user_input):
     global config
     # get the response from the API
-    response = openai.ChatCompletion.create(
-        engine=config["api_model"],
-        messages=[{"role": "system", "content": SHELL_PROMPT}, {
-            "role": "user", "content": user_input}
+    global client
+    response = client.chat.completions.create(
+        model=config["api_model"],
+        messages=[
+            {"role": "system", "content": SHELL_PROMPT},
+            {"role": "user", "content": user_input},
         ],
         temperature=0.7,
         max_tokens=800,
         top_p=0.95,
         frequency_penalty=0,
         presence_penalty=0,
-        stop=None)
-    # return the response
-    if "choices" not in response:
-        return ""
-    choice = response["choices"][0]  # type: ignore
-    if "message" not in choice:
-        return ""
-    message = choice["message"]
-    if "content" in message and "role" in message and message["role"] == "assistant":
-        return message["content"]
-    return ""
+        stop=None,
+    )
+    return response.choices[0].message.content
 
 
-# def runCommand(command):
-#     # if mac or linux
-#     if os_name == "Darwin/MacOS" or os_name == "Linux":
-#         os.system(command)
-#     # if windows
-#     elif os_name == "Windows":
-#         os.system("pwsh " + command)
-
-def main():
+def init_config_and_client():
     global config
+    global client
     root = sys.argv[1]
     # load config from openai_config.yml
     # open yml file
@@ -78,6 +68,21 @@ def main():
     for key, value in config.items():
         setattr(openai, key, value)
 
+    # init openai client
+    if "api_type" in config and config["api_type"] == "azure":
+        client = openai.AzureOpenAI(
+            api_key=config["api_key"],
+            azure_endpoint=config["api_base"],
+            api_version=config["api_version"],
+        )
+    else:
+        client = openai.OpenAI(
+            api_key=config["api_key"],
+            api_base=config["api_base"],
+        )
+
+def main():
+    init_config_and_client()
     # turn the args into a single string
     args = " ".join(sys.argv[2:])
     # get the response from the API
@@ -88,15 +93,7 @@ def main():
         exit()
     # print the response
     print(response)
-    # # ask the user if they want to execute the command, default is yes
-    # execute = input("Execute? [Y/n] ").lower()
-    # # if the user wants to execute the command
-    # if execute == "" or execute == "y" or execute == "yes":
-    #     # execute the command
-    #     runCommand(response)
 
 
 if __name__ == "__main__":
     main()
-
-    
