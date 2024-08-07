c2460e3e715b10521c837501d01c9b3ee7dc2f53
Xiaochen Wu <xiaocw@microsoft.com>
2024-08-07 09:59:06 +0800

    update install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ cd $HOME
 # Clone the repo
 git clone https://github.com/billxc/cloud_shell.git .cloud_shell
 
-.cloud_shell
+cd .cloud_shell
 # Execute the init.ps1 in the .cloud_shell folder
 & './init.sh'
