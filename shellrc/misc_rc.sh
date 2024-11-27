d582c041b3289310525822f205ec7b2dd5818fef
Xiaochen <wxc9312@gmail.com>
2024-11-27 18:18:12 +0800

    add daemon function for continuous command execution

--- shellrc/misc_rc.sh
+++ shellrc/misc_rc.sh
@@ -1,3 +1,12 @@
 hist() {
   omz_history | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' | awk '{$1=$1};1' | sort | uniq | fzf
 }
+
+daemon() {
+  while true
+  do
+    # run the command
+    $@
+  sleep 1
+  done
+}
